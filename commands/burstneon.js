16ef54211dc154882848d0f75a83b9ded23f9b63
Zach Van <Zach@zachvan.com>
2018-07-16 21:06:49 -0400

    Updated -burstneon

--- commands/burstneon.js
+++ commands/burstneon.js
@@ -5,23 +5,16 @@ module.exports = {
         message.delete()
         .then(msg => console.log(`Deleted message from ${msg.author}`))
         .catch(console.error);
+        var text = `Server: burstneon.com
+        Port: 8080
+        Reccomended Deadline Limit: 63072000 (This is 2 years)
+        If using qbundle wallet, go to: __T__ools -> Miner
+        If using the command line in 'miner-burst' folder: edit miner.conf`;
         if(message.mentions.users.size === 0) {
-             message.reply(`BURSTNEON MINER CONFIGURATION SETTINGS:
-Server: burstneon.com
-Port: 8080
-Reccomended Deadline Limit: 63072000 (This is 2 years)
-If using qbundle wallet, go to: __T__ools -> Miner
-If using the command line in 'miner-burst' folder: edit miner.conf
-Need more? See:  https://www.sansun.no/burstneon.php`);
+             message.reply(text);
            } else{
                 let TagedMember = message.guild.member(message.mentions.users.first());
-               message.channel.send(`${TagedMember} BURSTNEON MINER CONFIGURATION SETTINGS:
-Server: burstneon.com
-Port: 8080
-Reccomended Deadline Limit: 63072000 (This is 2 years)
-If using qbundle wallet, go to: __T__ools -> Miner
-If using the command line in 'miner-burst' folder: edit miner.conf
-Need more? See:  https://www.sansun.no/burstneon.php`);
+               message.channel.send(`${TagedMember} ${text}`);
            }
     },
 };
