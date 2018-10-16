9cfc0e3aa97112a0c2091141f49ad512449ff93f
Zach Van <Zach@zachvan.com>
2018-10-15 21:52:28 -0400

    update to start

--- commands/start.js
+++ commands/start.js
@@ -5,14 +5,15 @@ module.exports = {
         message.delete()
         .then(msg => console.log(`Deleted message from ${msg.author}`))
         .catch(console.error);
-        var text = `Here is the proper logic:
-        Get a coin! From ether this discord server (kindly ask someone) or from a facuet
-        1) Set reward recipient: https://burstneon.com/rewardassignment.html
-            For burstneon.com, recipient is: BURST-YXZW-JH7M-QKR9-9PKBN
-        2) Mine and earn some coins.
-        3) Set your name when you finally have some coins.
-        In other words, you can mine without setting your name. Don't waste your first coin on that.
-        Let setting the reward assignment be yout first transaction.`;
+        var text = `1) Set Reward Recipient: https://burstneon.com/rewardassignment.html
+        This costs only 0.1 burst. 
+        Burstneon pool account reward recpient is: BURST-YXZW-JH7M-QKR9-9PKBN
+    2) Setting your name in your wallet only costs 0.1 burst
+    
+    As POC2 is now in effect you need to be using POC2 miner with POC2 plots
+    Latest Blago recommend https://github.com/JohnnyFFM/blagominer/releases
+    Neon Pools Dashboard http://burstneon.com/ this also has the full guide
+    to bursting and to do everything and troubleshoot http://burstneon.com/qbundle.html.`;
         if(message.mentions.users.size === 0) {
              message.reply(text);
            } else{
